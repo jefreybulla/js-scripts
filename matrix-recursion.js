39f8d04a48467dd3f33e2357ca53eb3d5b8fbc6c
Jefrey Bulla <jefrey.bulla@gmail.com>
2024-08-29 14:41:43 -0400

    add approach

--- matrix-recursion.js
+++ matrix-recursion.js
@@ -1,5 +1,5 @@
 /*\
-Given an imaginary matrix where each element is the result of the left and top element
+Given an imaginary matrix where each element is the result of the sum of the left and top element
 
 1.  1.  1.  1.  1
 1   2.  3.  4.  5
@@ -7,25 +7,63 @@ Given an imaginary matrix where each element is the result of the left and top e
 1   4.  10. 20. 35
 1   5.  15. 35. 70
 
-Calculate element in specified location
+Create a function to calculate element in specified location
 Input (2, 2) => 6
 
 */
-
 //        (2,2)
 //   (2,1)  +   (1,2) 
 //(2,0)+(1,1)  (1,1)+(0,2)
 
-
-const myFunction = function(a,b){
-     console.log(`a: ${a} b: ${b}`)
+const calculateElement = function(a,b){
     if (a <=0 || b <=0){
         return 1
-    }    
-    return myFunction(a, b-1) + myFunction(a-1, b)
+    }  
+    return calculateElement(a, b-1) + calculateElement(a-1, b)
 }
 
-let a = 1
-let b = 4      // output: 5
-let result = myFunction(a,b)
+let a = 12
+let b = 12     // output: 70
+let start = performance.now()
+let result = calculateElement(a,b)
+let finish = performance.now()
 console.log(result)
+console.log(`execution time: ${finish-start}`)
+// Time complexity: exopnential O(2^n)since the growth rate doubles with each addition to the input
+
+// Approach 2: build matrix with loops
+console.log('approach 2 ->')
+const calculateElement2 = function(a,b){
+    let sizeOfMatrix = Math.max(a,b) + 1
+    let resultArray = []
+    let firstRow = new Array(sizeOfMatrix).fill(1)
+    // create first row 
+    resultArray[0] = firstRow
+    // initialize create other rows
+    for(let i=1; i<sizeOfMatrix; i++){
+        let newRow = new Array(sizeOfMatrix).fill(0)
+        newRow[0] = 1
+        resultArray.push(newRow)
+    }
+    //console.log(resultArray)
+    // calculate all other elements of matrix
+    for(let i=1; i<=a; i++){
+        for(let j=1; j<=b; j++){
+            resultArray[i][j] = resultArray[i-1][j] + resultArray[i][j-1]
+        }
+    }
+    //console.log(resultArray)
+    return resultArray[a][b]
+}
+
+start = performance.now()
+result = calculateElement2(a,b)
+finish = performance.now()
+console.log(result)
+console.log(`execution time: ${finish-start}`)
+/*
+Time complexity is O(a*b) which is better than O(2^n)
+For a = 12 and b = 12 
+Approach 1: 27ms
+Approach 2: 0.1ms an order of magnitude faster!
+*/
