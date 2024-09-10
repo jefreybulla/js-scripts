df3b6bac3522dce5d1a1b7ea65109de5a87333bb
Jefrey Bulla <jefrey.bulla@gmail.com>
2024-09-10 14:40:47 -0400

    add new approach to lonely-integer

--- lonely-integer.js
+++ lonely-integer.js
@@ -20,7 +20,37 @@ function lonelyInteger(param) {
 }
 
 
-input = [1,1,2,2,3,4,4]
-
-result = lonelyInteger(input)
+let input = [1,1,2,2,3,4,4]
+let start = performance.now()
+let result = lonelyInteger(input)
+let finish = performance.now()
 console.log(result)
+console.log(`execution time: ${finish-start}`)
+
+// using the Map object
+function lonelyInteger2(param) {
+    // Write your code here
+    const results = new Map()
+
+    for(let i of param){
+        if (!results.get(i)){
+            results.set(i,1)
+        }
+        else{
+            results.set(i,results.get(i)+1)
+        }
+    }
+    for (let [key, value] of results){
+        if(value == 1){
+            return key
+        }
+    }
+}
+
+input = [1,1,2,2,3,4,4]
+start = performance.now()
+result = lonelyInteger2(input)
+finish = performance.now()
+console.log(result)
+console.log(`execution time: ${finish-start}`)
+// both approaches have similar execution time
