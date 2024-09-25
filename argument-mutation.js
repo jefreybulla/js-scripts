f6e9dcf96c80d69d44e485b431ffaaf035e08808
Jefrey Bulla <jefrey.bulla@gmail.com>
2024-09-25 12:40:52 -0400

    improve argument-mutation script

--- argument-mutation.js
+++ argument-mutation.js
@@ -1,8 +1,22 @@
-/* 
-When using some methods such as .sort inside a function we have be careful 
-not to mutate the argument used as input
+/*
+When using primmitive types as arguments (string, number, boolean. etc), the original value is not mutated when passed to a function.
 */
 
+function simpleStringChange(myParam){
+    myParam = myParam + 'c'
+    return myParam
+}
+
+myArgument = 'ab'
+console.log(myArgument)     // ab
+result = simpleStringChange(myArgument)
+console.log(`result: ${result}`)    // a
+console.log(myArgument)     // ab
+
+/* 
+When passing arguments of non-primitive type (objects, arrays) to a function we have be careful 
+not to mutate the argument within the function.
+*/
 
 function firstElementSorted (myParameter){
     myParameter.sort()
@@ -19,7 +33,7 @@ console.log(myArgument)     // [a,b]  We may not want this!!
 // To avoid undesired mutations we can use structureClone or Object.assign to clone the object
 
 function firstElementSorted2 (myParameter2){
-    // In some cases Object.assign([], m) will mutate the solurce in the context of the function
+    // In some cases Object.assign([], m) will mutate the source in the context of the function
     // So a safer alternative is using deep cloning with structuredClone()
     // see flipping-matrix.js for an example of that situation
     let paramCopy = structuredClone(myParameter2)
