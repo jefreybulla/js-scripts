90eb4789a281e08e7d4cb747272ad10efca06434
Jefrey Bulla <jefrey.bulla@gmail.com>
2025-03-27 10:45:45 -0400

    add new methods to LinkList class

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,11 @@
 "use strict";
+/* Linked lists are time efficent compared to Arrays for
+- removal of any element that is not the last element
+- insertion of elements that is not the last element
+
+Arrays are more efficient at adding or removing the last element
+*/
+
 const Node = require('./Node.js');
 
 module.exports = class LinkedList {
@@ -24,11 +31,11 @@ module.exports = class LinkedList {
       console.log("Empty List");
       return false;
     } else {
-      let temp = this.head;
-      while (temp != null) {
-        process.stdout.write(String(temp.data));
+      let currentNode = this.head;
+      while (currentNode != null) {
+        process.stdout.write(String(currentNode.data));
         process.stdout.write(" -> ");
-        temp = temp.nextElement;
+        currentNode = currentNode.nextElement;
       }
       console.log("null");
       return true;
@@ -38,10 +45,13 @@ module.exports = class LinkedList {
   getHead() {
     return this.head;
   }
-  setHead(newHead) {
+  // time complexity O(1) is efficent at removing any number of items from the beggining of the list. 
+  // For comparison Array.shift has time complexity O(n)!
+  setHead(newHead) {            
     this.head = newHead;
     return this;
   }
+
   getListStr() {
     if (this.isEmpty()) {
       console.log("Empty List");
@@ -58,7 +68,8 @@ module.exports = class LinkedList {
       return st;
     }
   }
-  insertAtTail(newData) {
+
+  insertAtTail(newData) {     // O (n)
     //Creating a new Node with data as newData
     let node = new Node(newData);
 
@@ -81,20 +92,37 @@ module.exports = class LinkedList {
     currentNode.nextElement = node;
     return this;
   }
-  search(value) {
+
+  search(value) {               // time complexity: O(n)
     //Start from the first element
     let currentNode = this.head;
 
     //Traverse the list until you find the value or reach the end
     while (currentNode != null) {
       if (currentNode.data == value) {
-        return true; //value found
+        //return true; //value found
+        return currentNode        // return node that has queried value
       }
       currentNode = currentNode.nextElement
     }
     return false; //value not found
   }
-  deleteAtHead() {
+  // O(n) since we need to search
+  // O(1) for insertion. Efficent compared to Array.splice()
+  insertAtValue(value, nextValue){
+    let currentNode = this.search(value)
+    let newNode = new Node(nextValue)
+
+    // Check if value exists in list
+    if(!currentNode){
+      return false
+    }
+    newNode.nextElement = currentNode.nextElement
+    currentNode.nextElement = newNode
+    return this
+  }
+
+  deleteAtHead() {                // O(1) efficent compared to Array.shift O(n)
     //if list is empty, do nothing
     if (this.isEmpty()) {
       return this;
@@ -107,7 +135,10 @@ module.exports = class LinkedList {
 
     return this;
   }
-  deleteVal(value) {
+
+  // O(n) since we need to search
+  // O(1) for element removal. Efficent compared to Arra.splice() that is O(n)
+  deleteVal(value) {              
     let deleted = null; //True or False
 
     //if list is empty return false
@@ -136,7 +167,8 @@ module.exports = class LinkedList {
     deleted = false;
     return deleted;
   }
-  deleteAtTail() {
+
+  deleteAtTail() {            // O(n)   inefficient compared to Array.push that is O(1)
     // check for the case when linked list is empty
     if (this.isEmpty()) {
       return this;
